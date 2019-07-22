e9d3fd6c82bfc0a7012c3fc44c1a4ce203a5454d
Zhanna Gaidukova <zhanna.gaidukova@devexpress.com>
2019-07-22 14:44:06 +0300

    Modify sample: change DataBindings to ExpressionBindings, change report DataSet to SqlDataSource, update reports merging procedure

--- CS/PageMerging/Form1.cs
+++ CS/PageMerging/Form1.cs
@@ -12,27 +12,36 @@ namespace PageMerging {
         public Form1() {
             InitializeComponent();
         }
-
         private void simpleButton1_Click(object sender, EventArgs e) {
-            MainReport report = new MainReport();
+            XtraReport mainReport = CreateMainReport();                       
+            mainReport.ShowPreview();
+        }
+
+        private void MainReport_AfterPrint(object sender, EventArgs e) {
+            XtraReport additionalReport = CreateAdditionalReport();
+            (sender as XtraReport).ModifyDocument(x => { x.AddPages(additionalReport.Pages); });
+        }
+
+        private XtraReport CreateMainReport() {
+            MainReport mainReport = new MainReport();
+            mainReport.AfterPrint += MainReport_AfterPrint;
             DataSet1 ds = new DataSet1();
             Random rnd = new Random(DateTime.Now.Millisecond);
-            for (int i = 0; i < 50; i++)
-            {
+            for(int i = 0; i < 50; i++) {
                 int OrderID = rnd.Next(100);
                 int ProductID = rnd.Next(50);
-                if (ds.Order_Details.FindByOrderIDProductID(OrderID, ProductID) == null)
-                    ds.Order_Details.AddOrder_DetailsRow(OrderID, ProductID, (decimal)Math.Round(rnd.NextDouble()*50, 2), 5, 0.0f);
+                if(ds.Order_Details.FindByOrderIDProductID(OrderID, ProductID) == null)
+                    ds.Order_Details.AddOrder_DetailsRow(OrderID, ProductID, (decimal)Math.Round(rnd.NextDouble() * 50, 2), 5, 0.0f);
             }
-            report.DataSource = ds;
-            report.AfterPrint += new EventHandler(report_AfterPrint);
-            report.ShowPreview();            
+            mainReport.DataSource = ds;
+            mainReport.CreateDocument();
+            return mainReport;
         }
 
-        void report_AfterPrint(object sender, EventArgs e) {
+        private XtraReport CreateAdditionalReport() {
             AdditionalReport add = new AdditionalReport();
             add.CreateDocument();
-            ((MainReport)sender).Pages.AddRange(add.Pages);
-        }
+            return add;
+        }        
     }
 }
